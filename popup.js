053d11d26cc885bd7688fd15aeed4efd2c39f979
Zhiyu <zhiyu.qi@gmail.com>
2017-02-21 22:32:35 +1300

    revise entry function

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+﻿// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -92,27 +92,52 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+/**
+ * @param {url} listing Url, could be in many forms
+ * verify listing url and return Id if found, otherwise 0
+ */
+function extractListingId(url){
+    //http://www.trademe.co.nz/property/residential-property-for-sale/auction-1249809896.htm
+    //http://www.trademe.co.nz/Browse/Listing.aspx?id=1256906561
+    //http://www.trademe.co.nz/1256906561
+    return 0;
+}
+
+function getHistory(url, callback, errorCallback) {
+    var id = extractListingId(url);
+    if (id == 0)
+        errorCallback();
+
+    //TODO: load history from API
+    var searchUrl = "";
+    var x = new XMLHttpRequest();
+    x.open('GET', searchUrl);
+    // The Google image search API responds with JSON, so let Chrome parse it.
+    x.responseType = 'json';
+    x.send();
+}
+//loader entry
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    renderStatus('Performing history search for ' + url);
+    getHistory(url, null, null);
+    //getImageUrl(url, function(imageUrl, width, height) {
 
-    getImageUrl(url, function(imageUrl, width, height) {
+    //  renderStatus('Search term: ' + url + '\n' +
+    //      'Google image search result: ' + imageUrl);
+    //  var imageResult = document.getElementById('image-result');
+    //  // Explicitly set the width/height to minimize the number of reflows. For
+    //  // a single image, this does not matter, but if you're going to embed
+    //  // multiple external images in your page, then the absence of width/height
+    //  // attributes causes the popup to resize multiple times.
+    //  imageResult.width = width;
+    //  imageResult.height = height;
+    //  imageResult.src = imageUrl;
+    //  imageResult.hidden = false;
 
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
+    //}, function(errorMessage) {
+    //  renderStatus('Cannot display image. ' + errorMessage);
+    //});
   });
 });
