c27ba5526b2b884103c1f4f1e8788d9f243ac8b4
Zhiyu <zhiyu.qi@gmail.com>
2017-02-23 22:30:00 +1300

    version 0.01

--- popup.js
+++ popup.js
@@ -100,28 +100,61 @@ function extractListingId(url){
     //http://www.trademe.co.nz/property/residential-property-for-sale/auction-1249809896.htm
     //http://www.trademe.co.nz/Browse/Listing.aspx?id=1256906561
     //http://www.trademe.co.nz/1256906561
-    return 0;
+    var patt = /trademe\D+(\d+)/i;
+    var matchs = patt.exec(url);
+    if (matchs == null || matchs.length != 2)
+        return 0;
+    return Number(matchs[1]);
 }
 
 function getHistory(url, callback, errorCallback) {
     var id = extractListingId(url);
-    if (id == 0)
-        errorCallback();
+    if (id == 0){
+        errorCallback("Invalid Url");
+        return;
+    }
 
     //TODO: load history from API
-    var searchUrl = "";
+    var searchUrl = "http://192.168.1.9/bo"+"/api/houses/"+String(id);
     var x = new XMLHttpRequest();
     x.open('GET', searchUrl);
     // The Google image search API responds with JSON, so let Chrome parse it.
     x.responseType = 'json';
+    x.onload = function () {
+        // Parse and process the response from Google Image Search.
+        var response = x.response;
+        if (!response) {
+            errorCallback('Listing not found!');
+            return;
+        }
+        callback(response.HouseHistories);
+    };
+    x.onerror = function () {
+        errorCallback('Network error.');
+    };
     x.send();
 }
+
+function renderResult(results) {
+    //clear display area
+    var status = document.getElementById('status');
+    status.innerHTML = "";
+    if (results == null || results.length == 0)
+        return;
+    
+    var text = "";
+    for (var i = 0; i < results.length; i++) {
+        text += String(results[i].ChangedAt) + "&nbsp;"+ results[i].Price + "<br/>";
+    }
+    status.innerHTML = text;
+}
+
 //loader entry
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
     renderStatus('Performing history search for ' + url);
-    getHistory(url, null, null);
+    getHistory(url, renderResult, renderStatus);
     //getImageUrl(url, function(imageUrl, width, height) {
 
     //  renderStatus('Search term: ' + url + '\n' +
---
+++ test.js
@@ -0,0 +1,71 @@
+﻿function test() {
+    testExtractListingId();
+    testRenderResult();
+}
+
+function testExtractListingId() {
+    var input = ["http://www.trademe.co.nz/property/residential-property-for-sale/auction-1249809896.htm",
+            "http://www.trademe.co.nz/Browse/Listing.aspx?id=1256906561",
+            "http://www.trademe.co.nz/1256906561",
+            "http://www.trademe.co.nz/antiques-collectables"
+    ];
+    var expect = [1249809896, 1256906561, 1256906561, 0];
+    for (var i = 0; i < input.length;i++) {
+        if (extractListingId(input[i]) != expect[i])
+            console.log("Error in testExtractListingId: " + input);
+    }
+}
+
+function testRenderResult() {
+    var result = {
+        "HouseHistories": [
+          {
+              "HouseHistoryId": 17154,
+              "HouseId": 1262288115,
+              "Price": "Price by negotiation",
+              "LastPrice": null,
+              "CreatedAt": "2017-02-15T21:50:50.613",
+              "ChangedAt": "2017-02-14T14:59:16.28"
+          }
+        ],
+        "HouseId": 1262288115,
+        "Title": "A Family Home That Offers It All",
+        "Address": "6 Deane Avenue, Titirangi, Waitakere City",
+        "Price": "Price by negotiation",
+        "Room": "4",
+        "BathRoom": "2",
+        "ListedAt": "2017-02-14T14:45:00",
+        "Auction": null,
+        "Land": null,
+        "PropertyType": "House",
+        "Agent": "Agent's\r\n                    details\n\n\n\n\n\nSteve Isbill\n\n(09) 8397469\n(027) 4927548\n\n\n\n\n\n\n\n\nHarveys Te Atatu - Elysium Realty Ltd MREINZ, Licensed Agent (REAA 2008)\n(09) 8346155\n\n\nOffice's other listings\nView their website",
+        "Wording": "\r\n    \r\n    If you are seeking a fantastic, spacious family home with spectacular views, offering real privacy, plenty of garaging and storage, then look no further. \r\n \r\nCome along and discover this massive, extensive timber bungalow set commandingly on a full, freehold site offering striking elevated views of the Waitakere Ranges while being neatly positioned in a quaint, private cul de sac location. \r\n \r\nComprising of four bedrooms, a huge family room with those stunning views, a large bathroom and a second lounge with an ample, secluded decked area, makes it the perfect place for those who like to entertain in privacy. \r\n \r\nOn the lower level, with internal access, is the extensive garaging that is clean and tidy, a second shower and toilet, not to mention the generously sized workshop and storage area. \r\n \r\nPlus, for those that love to renovate it's a wonderful property that you could easily get your teeth into, stamp your mark on and make some improvements to modernise and add real value. \r\n \r\nFor guaranteed easy commuting it is set in one of the most popular streets on the city side of Titirangi Village and being surrounded by excellent schooling, this fantastic home is sure to be extremely popular. \r\n \r\nContact Steve today. \r\n \r\nSET DATE OF SALE Closes 4pm, Wednesday 8 March 2017 (unless sold prior)\r\nAgency reference #: TAT3441\r\n    \r\n    \r\n    \r\n    \r\n    \r\n    \r\n    \r\n",
+        "Created": "2017-02-14T14:59:16.28",
+        "Url": "/property/residential-property-for-sale/auction-1262288115.htm",
+        "ImageUrl": "http://trademe.tmcdn.co.nz/photoserver/med/556169277.jpg",
+        "Feature": " feature highlight",
+        "History": null,
+        "LastVisit": "2017-02-15T21:34:59.617"
+    };
+    var output = "2017-02-14T14:59:16.28&nbsp;Price by negotiation<br>";
+
+    var status = document.getElementById('status');
+    if (status == null){
+        var div = document.createElement('div');
+        document.body.appendChild(div);
+        div.id = 'status';
+        div.style.position = 'fixed';
+        div.style.top = '50%';
+        div.style.left = '50%';
+        div.style.width = '100%';
+        div.style.height = '100%';
+        div.style.backgroundColor = 'red';
+    }
+
+    renderResult(result.HouseHistories);
+    status = document.getElementById('status');
+
+    if (status.innerHTML != output)
+        console.log("Error in testRenderResult" );
+
+}
